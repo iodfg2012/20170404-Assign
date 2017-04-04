25d3492c9eb73afbb0dfee358c15f3e07b9de864
iodfg2012 <iodfg2012@daum.net>
2017-04-04 15:58:22 +0900

    문제3번 Cylinder
    Cylinder 코드 재수정

--- javaOO/q3.java
+++ javaOO/q3.java
@@ -1,20 +1,16 @@
 public class Cylinder{
 	
 	private double height;
-
-	public Cylinder(Circle cir ,double height) {
-		this.height = height;
-		
-	}
 	
-	public double getVolume(Circle cir, double height){
+	public double getVolume(Circle cir){
 		return cir.getArea() * height;
 	}
 	
 	public static void main(String[] args) {
 		Circle cir = new Circle(2.8);
-		Cylinder cd = new Cylinder(cir, 5.6);
-		System.out.println("원 기둥 부피 : " + cd.getVolume(cir, 5.6));
+		Cylinder cd = new Cylinder();
+		cd.height = 5.6;
+		System.out.println("원 기둥 부피 : " + cd.getVolume(cir));
 	
 	}
 	
